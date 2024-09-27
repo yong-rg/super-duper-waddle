61e205fcd9cff574a6e12a3f9eec71a78a426b32
Yong Los <yong.los@riverguid.io>
2024-09-27 13:07:34 -0400

    first commit

---
+++ zeiss_pipeline.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+amz_url = 'https://docs.google.com/spreadsheets/d/1pP8jJDYPS1nHjHHDM1IycjtdFLLJIA8kRQ5AaC32Su0/edit?gid=968213497#gid=968213497'
+google_url = 'https://docs.google.com/spreadsheets/d/1oADSDDLjFmVs5tkc6KxJQW5B0VRg1qxaSqtQSV6EemA/edit?gid=366997270#gid=366997270'
+
